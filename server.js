53c53323139df5160606981c2b73ad4b6e4cadaa
DevolperRL <100154298+DevolperRL@users.noreply.github.com>
2024-12-01 18:17:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const WebSocket = require("ws");
 
 // Create an Express application
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Serve static files if needed (optional)
 app.use(express.static("public"));
